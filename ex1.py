e7438f61f8d223bc41c1db075df8069e8bc5c32c
stern15 <ssemasuka@gmail.com>
2016-02-02 15:33:51 +0300

    a simple eif and else in pytho

--- ex1.py
+++ ex1.py
@@ -1,1 +1,9 @@
-print ("yeahhhhh")
+a = 4
+b = 5
+if a > b:
+    print("a({})is great than is({})".format(a, b))
+else:
+    print("a({}) is le ss than b({})".format(a, b))
+
+
+print("a" if a > b else "b")
